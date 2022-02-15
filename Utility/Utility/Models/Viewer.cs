582116a840f312bdbfce5af329659ed02e3a0ab6
boltik39 <bakashapov2001@yandex.ru>
2022-02-16 01:31:17 +0300

    Find mistake in word

--- Utility/Utility/Models/Viewer.cs
+++ Utility/Utility/Models/Viewer.cs
@@ -29,7 +29,7 @@ public class Viewer
 
     private void ViewerBody(object? state)
     {
-        LogHelper.Info("Start perioudly checking");
+        LogHelper.Info("Start periodic checking");
         
         if (!WaitAndFindProcess(_process))
             return;
